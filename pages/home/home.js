be7e2fb724cde041de94ee9a7de97d3178510561
tuzhenfeng <tuzhenfeng929@163.com>
2019-12-05 10:04:29 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+// import request from '/service/network.js'
 Page({
 
   /**
@@ -12,7 +13,52 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 1.原生方式发送网络请求
+    this.get_data_origin()
+    // 2.使用封装的request发送网络请求
+    // promise最大的好处就是放值出现回调地狱
+    // request({
+    //   url:'http://106.54.54.237:8000/api/v1/recommend',
+    // }).then(res=>{
+    //   console.log(res)
+    // }).catch(err=>{
+    //   console.log(err)
+    // })
+  },
+  get_data_origin(){
+    // 1.最简单的get请求
+    wx.request({
+      url: 'http://106.54.54.237:8000/api/v1/recommend',
+      success: function (res) {
+        console.log(res)
+      }
+    })
+    // 2.get请求，携带参数
+    // wx.request({
+    //   url: 'http://106.54.54.237:8000/api/v1/home/data',
+    //   data:{
+    //     type:'sell',
+    //     page:1
+    //   },
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+    // 3.post请求，并且携带参数
+    // wx.request({
+    //   url: 'http://106.54.54.237:8000/api/v1/home/data',
+    //   methods: 'post',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success: function (res) {
+    //     console.log(res)
+    //   },
+    //   fail:function(err){
+    //     console.log(err)
+    //   }
+    // })
   },
 
   /**
